6c5fc62e32ae54e750d5cd500a2bbe7a816032c5
2022karanbhasin <44887140+2022karanbhasin@users.noreply.github.com>
2020-06-06 01:40:43 +0530

    port from environment variable

--- auth-serverfinal/authorization_code/app.js
+++ auth-serverfinal/authorization_code/app.js
@@ -145,5 +145,7 @@ app.get('/refresh_token', function(req, res) {
   });
 });
 
-console.log('Listening on 8888');
-app.listen(8888);
+const port=process.env.PORT || 8888;
+
+console.log(`Server starting on ${port}`);
+app.listen(port);
